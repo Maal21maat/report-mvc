ad138040fed31fad22dcc9d97dfd2a3b9b91643a
Maal21maat <maat21@student.bth.se>
2023-05-12 11:02:13 +0200

    Arkiverat Gameklassen, spras till kmom03

--- src/Card/Game.php
+++ src/Card/Game.php
@@ -4,23 +4,23 @@ namespace App\Card;
 
 class Game
 {
-    private $deck;
-    private $hand;
+    // private $deck;
+    // private $hand;
 
-    public function __construct() {
-        $this->deck = new Deck();
-        $this->deck->shuffle();
-        $this->hand = new Hand();
-    }
+    // public function __construct() {
+    //     $this->deck = new Deck();
+    //     $this->deck->shuffle();
+    //     $this->hand = new Hand();
+    // }
 
-    public function drawCard($numCards = 1) {
-        $cards = $this->deck->draw($numCards);
-        foreach ($cards as $card) {
-            $this->hand->add($card);
-        }
-    }
+    // public function drawCard($numCards = 1) {
+    //     $cards = $this->deck->draw($numCards);
+    //     foreach ($cards as $card) {
+    //         $this->hand->add($card);
+    //     }
+    // }
 
-    public function getHand() {
-        return $this->hand;
-    }
+    // public function getHand() {
+    //     return $this->hand;
+    // }
 }
